85ec1d6d200424a5d6da052325291cb0c40934c0
xiaoqiyang <meetqy@qq.com>
2022-07-05 16:25:46 +0800

    模板详情增加loading

--- pages/template/detail/pro-[id].vue
+++ pages/template/detail/pro-[id].vue
@@ -1,11 +1,19 @@
 <template>
   <nuxt-layout name="tools">
-    <div class="tools-body overflow-hidden">
+    <div class="tools-body overflow-hidden relative">
       <iframe
         class="w-full h-full"
         :src="`${getUrl()}beauty-template/en/card/${file[1]}`"
         frameborder="0"
-      ></iframe>
+        @load="onLoad"
+      />
+
+      <div
+        class="w-full h-full left-0 top-0 absolute flex justify-center items-center bg-base-100 bg-opacity-50"
+        v-if="loading"
+      >
+        <img src="/loading.gif" />
+      </div>
     </div>
   </nuxt-layout>
 </template>
@@ -13,6 +21,12 @@
 <script setup>
 const route = useRoute();
 const { id } = route.params;
+
+const loading = ref(true);
+
+const onLoad = (e) => {
+  loading.value = false;
+};
 
 const getUrl = () =>
   useIsProducton() ? `https://wcao.cc/` : `http://localhost:3008/`;
@@ -23,16 +37,17 @@ const { data } = await useAsyncData(`posts/${id}`, () =>
   })
 );
 
-const post = computed(() => {
-  return data.value.data.attributes;
-});
+const post = computed(() => data.value.data.attributes);
 
 const file = post.value.title.split(" Part ");
 
 const ultraName = computed(() => ultra[file[1]]);
 
-onMounted(() => {
-  // console.log(ultraName.value);
+onMounted(async () => {
+  await nextTick();
+
+  loading.value = false;
+
   useHead({
     titleTemplate: `${post.value.title}:${ultraName.value[1]} - 模板`,
   });
